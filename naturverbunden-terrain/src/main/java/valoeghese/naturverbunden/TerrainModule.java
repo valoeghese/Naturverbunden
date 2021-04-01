c72771ffaf37dff7b661cfb48b8e1396d987724b
valoeghese <valoeghesemods@gmail.com>
2021-04-02 00:39:15 +1300

    fixes

---
+++ naturverbunden-terrain/src/main/java/valoeghese/naturverbunden/TerrainModule.java
@@ -0,0 +1,12 @@
+package valoeghese.naturverbunden;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TerrainModule implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		Naturverbunden.LOGGER.info("[Terrain] Initialising!");
+	}
+
+}
